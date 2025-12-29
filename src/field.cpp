df7547dfe62c72414a0625a1aa68e33db9d17542
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-12-29 20:41:18 +0300

    feat: introduce field class

---
+++ src/field.cpp
@@ -0,0 +1,103 @@
+#include "field.h"
+
+#include <cassert>
+#include <ostream>
+#include <utility>
+
+using namespace Jvm;
+
+
+Field::Field(ConstantUtf8Info* name, ConstantUtf8Info* descriptor) : name_(name), descriptor_(descriptor)
+{
+    Class* nameOwner = name->getClassOwner();
+    Class* descriptorOwner = descriptor->getClassOwner();
+    assert(nameOwner == descriptorOwner);
+    classOwner_ = nameOwner;
+}
+
+Field::Field(std::string name, std::string descriptor, Class* classOwner) : classOwner_(classOwner)
+{
+    name_ = ConstantUtf8Info::getOrCreate(std::move(name), classOwner);
+    descriptor_ = ConstantUtf8Info::getOrCreate(std::move(descriptor), classOwner);
+}
+
+void Field::addFlag(AccessFlag flag)
+{
+    accessFlags_.insert(flag);
+}
+
+void Field::removeFlag(AccessFlag flag)
+{
+    accessFlags_.erase(flag);
+}
+
+void Field::addAttribute(Attribute* attribute)
+{
+    attributes_.insert(attribute);
+}
+
+void Field::removeAttribute(Attribute* attribute)
+{
+    attributes_.erase(attribute);
+}
+
+const std::set<Field::AccessFlag>* Field::getAccessFlags() const
+{
+    return &accessFlags_;
+}
+
+ConstantUtf8Info* Field::getName() const
+{
+    return name_;
+}
+
+ConstantUtf8Info* Field::getDescriptor() const
+{
+    return descriptor_;
+}
+
+const std::set<Attribute*>* Field::getAttributes() const
+{
+    return &attributes_;
+}
+
+Class* Field::getClass() const
+{
+    return classOwner_;
+}
+
+
+void Field::toBinary(std::ostream& os) const
+{
+    // u2             access_flags;
+    uint16_t accessFlags = 0x0000;
+    for (auto flag : accessFlags_)
+    {
+        accessFlags = accessFlags | flag;
+    }
+    os.write(reinterpret_cast<const char*>(&accessFlags), sizeof(accessFlags));
+
+    // u2             name_index;
+    uint16_t nameIndex = name_->getIndex();
+    os.write(reinterpret_cast<const char*>(&nameIndex), sizeof(nameIndex));
+
+    // u2             descriptor_index;
+    uint16_t descriptorIndex = descriptor_->getIndex();
+    os.write(reinterpret_cast<const char*>(&descriptorIndex), sizeof(descriptorIndex));
+
+    // u2             attributes_count;
+    uint16_t attributeCount = attributes_.size();
+    os.write(reinterpret_cast<const char*>(&attributeCount), sizeof(attributeCount));
+
+    // attribute_info attributes[attributes_count];
+    for (auto attribute : attributes_)
+    {
+        os << attribute;
+    }
+}
+
+std::ostream& Jvm::operator<<(std::ostream& os, const Field& field)
+{
+    field.toBinary(os);
+    return os;
+}
---
+++ src/field.h
@@ -0,0 +1,112 @@
+#ifndef JVM__FIELD_H
+#define JVM__FIELD_H
+#include <iosfwd>
+
+#include "constant-utf-8-info.h"
+
+namespace Jvm
+{
+    class Attribute;
+
+    class Field
+    {
+        friend std::ostream& operator<<(std::ostream& os, const Field& field);
+
+    public:
+        enum AccessFlag
+        {
+            ACC_PUBLIC = 0x0001, ///< Declared public; may be accessed from outside its package.
+            ACC_PRIVATE = 0x0002,
+            ///< Declared private; accessible only within the defining class and other classes belonging to the same nest.
+            ACC_PROTECTED = 0x0004, ///< Declared protected; may be accessed within subclasses.
+            ACC_STATIC = 0x0008, ///< Declared static.
+            ACC_FINAL = 0x0010, ///< Declared final; never directly assigned to after object construction (JLS §17.5).
+            ACC_VOLATILE = 0x0040, ///< Declared volatile; cannot be cached.
+            ACC_TRANSIENT = 0x0080, ///< Declared transient; not written or read by a persistent object manager.
+            ACC_SYNTHETIC = 0x1000, ///< Declared synthetic; not present in the source code.
+            ACC_ENUM = 0x4000, ///< Declared as an element of an enum class.
+        };
+
+        /**
+         * Create field with selected name and descriptor.
+         * @param name Pointer to UTF-8 constant with field name.
+         * @param descriptor Pointer to UTF-8 constant with field descriptor.
+         * @note The name and descriptor must belong to the same  owner.
+         */
+        Field(ConstantUtf8Info* name, ConstantUtf8Info* descriptor);
+
+        /**
+         * Create field with selected name, descriptor and classOwner.
+         * @param name Field name string.
+         * @param descriptor Field descriptor string.
+         * @param classOwner Pointer to class owner.
+         */
+        Field(std::string name, std::string descriptor, Class* classOwner);
+
+        /**
+         * Add access flag to field.
+         * @param flag Access flag.
+         */
+        void addFlag(AccessFlag flag);
+
+        /**
+         * Remove access flag from field.
+         * @param flag Access flag.
+         */
+        void removeFlag(AccessFlag flag);
+
+        /**
+         * Add attribute to field.
+         * @param attribute Field attribute.
+         */
+        void addAttribute(Attribute* attribute);
+
+        /**
+         * Remove attribute from field.
+         * @param attribute Field attribute.
+         */
+        void removeAttribute(Attribute* attribute);
+
+        /**
+         * @return Access flags set.
+         */
+        [[nodiscard]] const std::set<AccessFlag>* getAccessFlags() const;
+
+        /**
+         * @return Constant of field name.
+         */
+        [[nodiscard]] ConstantUtf8Info* getName() const;
+
+        /**
+         * @return Constant of field descriptor.
+         */
+        [[nodiscard]] ConstantUtf8Info* getDescriptor() const;
+
+        /**
+         * @return Field attributes set.
+         */
+        [[nodiscard]] const std::set<Attribute*>* getAttributes() const;
+
+        /**
+         * @return Class owner.
+         */
+        [[nodiscard]] Class* getClass() const;
+
+    private:
+        /**
+         * Write object to binary stream.
+         * @param os Output stream.
+         */
+        void toBinary(std::ostream& os) const;
+
+        std::set<AccessFlag> accessFlags_{}; ///< Access flags.
+        ConstantUtf8Info* name_ = nullptr; ///< String constant with field name.
+        ConstantUtf8Info* descriptor_ = nullptr; ///< String constant with field descriptor.
+        std::set<Attribute*> attributes_{}; ///< Attributes.
+        Class* classOwner_ = nullptr;
+    };
+
+    std::ostream& operator<<(std::ostream& os, const Field& field);
+} // Jvm
+
+#endif //JVM__FIELD_H
